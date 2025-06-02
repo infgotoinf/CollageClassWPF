564f06cb877f109068dab1802670952efa4b4727
studentColl <ggxaoc@gmail.com>
2025-06-02 14:38:13 +0300

    Добавьте файлы проекта.

---
+++ 01 Introduction/Program.cs
@@ -0,0 +1,65 @@
+﻿//GUI
+/*
+ * .Net - WinForms and WPF
+ * 
+ * WIN FORMS
+ * 2002 - .Net Framework 1
+ *        (easy to use, visual constructor (VS), basic controls)
+ * 
+ * 2005 - .Net Framework 2
+ *        (new controls, theme support)
+ * 
+ * 2006-2007 - .Net Flamework 3
+ *             (WPF, WPF integration)
+ *             
+ * 2010+ - .Net Framework 4+, .Net Core
+ *         (perfomance, crossplatform try, open source code)
+ * 
+ * 
+ * Form - container what is a window (dialog or just a basic window)
+ * Controls
+ * Event handling
+ * Form desighner
+ * 
+ * GDI
+ * 
+ * Pixel graphics
+ * 
+ * Event model
+ * 
+ * 
+ * +:
+ * - Easy to use
+ * - Fast to develop
+ * - Wide support
+ * - Compatibility
+ * 
+ * -:
+ * - Windows dependency
+ * - Aging opportunities (GDI)
+ * 
+ * 
+ * WPF
+ * Windows Presentation Foundation
+ * 2006-2007
+ * 2010+ - New controls, open source code, animations and 3D, crossplatform try, LINQ integration
+ * 
+ * Declerative approach (XAML)
+ * - Presentation (XAML)
+ * - Core
+ * - Data (LINQ)
+ * - Rendering (DirectX)
+ * - Integration
+ * 
+ * 
+ * +:
+ * - Vector graphics
+ * - Graphical opportunities
+ * - Easy to work with data (LINQ)
+ * 
+ * -:
+ * - Windows
+ * - Hard to use
+ * - Limited compatability with old .NET
+ * - Perfomance
+ */
