de8cc79b736e25273c842ca304c08705a0628423
studentColl <ggxaoc@gmail.com>
2025-06-09 14:55:05 +0300

    wpf

---
+++ 05 WPF Beggining/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textbox1.Text))
+            {
+                listbox1.Items.Add(textbox1.Text);
+                textbox1.Clear();
+            }
+        }
+
+    }
+}
